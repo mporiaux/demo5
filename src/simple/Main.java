fe0ff14ca6b17d0cc81a6191904f8e59a676cf99
mporiaux <michel.poriaux@condorcet.be>
2023-02-08 12:05:43 +0100

    classes métiers

---
+++ src/simple/Main.java
@@ -0,0 +1,17 @@
+package simple;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 0b0011;
+        int y = 0X1F;
+        int z= 045;
+        float a=  3.14F;
+        char c = '\u03B6';
+
+
+        System.out.println("x ="+x);
+        System.out.println("y ="+y);
+        System.out.println("z ="+z);
+        System.out.println("c = "+c);
+    }
+}
